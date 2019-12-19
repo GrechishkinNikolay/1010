d1f8aa1038685a9e6ddee801b145231da35dcbab
kolay <kolaygreha@mail.ru>
2019-12-19 11:06:40 +0300

    AddControllerWF

---
+++ StartBlockPuzzle/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StartBlockPuzzle
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new );
+    }
+  }
+}
+class Program
+{
+  private static void Main(string[] args)
+  {
+    ThreadsTest threadsTest = new ThreadsTest();
+    threadsTest.Execute();
+  }
+}
+
+class ThreadsTest
+{
+  private volatile bool IsRunning;
+
+  public void Execute()
+  {
+    IsRunning = true;
+    Thread th = new Thread(ThreadLoop);
+    th.IsBackground = true;
+
+    Console.WriteLine("MAIN: Starting thread");
+    th.Start();
+
+    Console.WriteLine("MAIN: Waiting");
+    Thread.Sleep(1000);
+
+    Console.WriteLine("MAIN: Stopping thread");
+    IsRunning = false;
+
+    Console.WriteLine("MAIN: Waiting for keypress to exit");
+    Console.ReadKey();
+  }
+  private void ThreadLoop()
+  {
+    Console.WriteLine("THREAD: Starting");
+    while (IsRunning)
+    {
+      Console.WriteLine("THREAD: Doing something");
+      Thread.Sleep(100);
+    }
+    Console.WriteLine("THREAD: Exiting");
+  }
+}
---
+++ ViewWindowsForms/ViewWindows.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViewWindowsForms
+{
+  class ViewWindows
+  {
+    /// <summary>
+    /// Рисование с использованием технологии двойной буферизации
+    /// </summary>
+    private BufferedGraphics _BufferedGraphics = null;
+    public RectangleF[][] FieldRectangles
+    {
+      get;
+      set;
+    }
+
+    public Form GameForm
+    {
+      get;
+      set;
+    }
+    private Thread thread;
+
+    public ViewWindows()
+    {
+      GameForm = new Form();
+      GameForm.FormBorderStyle = FormBorderStyle.FixedSingle;
+      Graphics targetGraphics = _PanelDrawing.CreateGraphics();
+      Rectangle targetRectangle = _PanelDrawing.ClientRectangle;
+      _BufferedGraphics = BufferedGraphicsManager.Current.Allocate(targetGraphics, targetRectangle);
+      FieldRectangles = new RectangleF[][];
+    }
+  }
+}
