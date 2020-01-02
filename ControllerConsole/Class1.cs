72290ae289d659162406f67dbe5ba6b8a51bab4d
kolay <kolaygreha@mail.ru>
2020-01-02 12:02:57 +0300

    2

---
+++ ControllerConsole/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerConsole
+{
+    public class Class1
+    {
+    }
+}
