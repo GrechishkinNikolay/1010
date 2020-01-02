1c9125ed73b5b3e6b935ccbe9bf1a411fb432d72
kolay <kolaygreha@mail.ru>
2020-01-02 13:34:36 +0300

    StartMenu

--- Model/ModelMenu.cs
+++ Model/ModelMenu.cs
@@ -8,5 +8,24 @@ namespace Model
 {
   public class ModelMenu
   {
+    /// <summary>
+    /// Выбранный пункт меню
+    /// </summary>
+    private int _selectedMenuItem;
+    /// <summary>
+    /// Свойство выбранного пункта меню
+    /// </summary>
+    public int SelectedMenuItem
+    {
+      get { return _selectedMenuItem; }
+      set
+      {
+        if (value >= 0 && value <= 3)
+        {
+          _selectedMenuItem = value;
+        }
+      }
+    }
+
   }
 }
